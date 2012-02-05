3ae026a73360d002c26dc541042acc5493822d80
Azer.xu <azer.xu@gmail.com>
2012-02-05 18:41:15 +0800

    fix the reproter method fit the change in libmp.py

--- test/test_libmp.py
+++ test/test_libmp.py
@@ -5,9 +5,7 @@
 # file: test_libmp.py
 # **********************************************************************
 
-from pyngs.lib.libmp import run
-# import time
-# import os
+from pyngs.lib.libmp import run, SENTINEL
 
 
 def producer(n):
@@ -20,8 +18,19 @@ def consumer(i):
     return i, i ** 3
 
 
-def reporter(i, j):
+def _reporter(i, j):
     print 'end:', i, j
+
+def reporter(iqueue, nconsumer=1, sentinel=SENTINEL):
+    while True:
+        item = iqueue.get()
+        if item is sentinel:
+            nconsumer -= 1
+            if nconsumer < 1:           # done
+                break
+            continue
+
+        _reporter(*item)
 
 
 if __name__ == '__main__':
